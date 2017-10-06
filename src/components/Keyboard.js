1d65d7b1ef58cf7873fb05aec3cf7a0b8dbf4491
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2017-10-06 18:31:15 +0200

    Basic keyboard

---
+++ src/components/Keyboard.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import bindAll from 'lodash/bindAll';
+
+const StyledKeyboard = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    height: 300px;
+    width: 500px;
+`;
+
+const StyledNote = styled.div`
+    width: calc(100% / ${props => props.noteAmount});
+    height: 100%;
+    background-color: red;
+    border: 1px solid black;
+`;  
+
+class Note extends Component {
+    constructor(props) {
+        super(props);
+        bindAll(this, 'onMouseDownHandler', 'onMouseUpHandler')
+    }
+
+    onMouseDownHandler(e) {
+        e.preventDefault();
+        this.props.onMouseDownHandler(this.props.i)
+    }
+
+    onMouseUpHandler(e) {
+        e.preventDefault();
+        this.props.onMouseUpHandler()
+    }
+
+    render() {
+        return (
+            <StyledNote 
+                noteAmount={this.props.noteAmount}
+                onMouseDown={this.onMouseDownHandler}
+                onMouseUp={this.onMouseUpHandler}
+            />
+        )
+    }
+} 
+
+export default class Keyboard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.keyMap = {
+            65: 'C5',
+            87: 'C#5',
+            83: 'D5',
+            69: 'D#5',
+            68: 'E5',
+            70: 'F5',
+            84: 'F#5',
+            71: 'G5',
+            89: 'G#5',
+            90: 'G#5',
+            72: 'A5',
+            85: 'A#5',
+            74: 'B5'
+        }
+
+        this.state =  {
+            notes: Object.values(this.keyMap)
+        }
+
+        bindAll(this, 'onKeyUpHandler', 'onKeyDownHandler', 'onMouseDownHandler', 'onMouseUpHandler');
+    }
+
+    onKeyUpHandler(e) {
+        e.preventDefault();
+    }
+
+    onKeyDownHandler(e) {
+        e.preventDefault();
+    }
+
+    onMouseDownHandler(key) {
+        this.props.synth.instance.triggerAttack(key);
+    }
+
+    onMouseUpHandler(e) {
+        this.props.synth.instance.triggerRelease();
+    }
+
+    componentWillMount() {
+        document.addEventListener('keydown', this.onKeyDownHandler);
+        document.addEventListener('keyup', this.onKeyUpHandler);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.onKeyDownHandler);
+        document.removeEventListener('keyup', this.onKeyUpHandler);
+    }
+
+    render() {
+        return (
+            <StyledKeyboard>
+                {this.state.notes.map((note, i) => 
+                    <Note 
+                        i={i}
+                        key={i} 
+                        onMouseDownHandler={this.onMouseDownHandler} 
+                        onMouseUpHandler={this.onMouseUpHandler} 
+                        noteAmount={this.state.notes.length} 
+                    />
+                )}
+            </StyledKeyboard>
+        )
+    }
+}
--- src/components/Synthesizer.js
+++ src/components/Synthesizer.js
@@ -2,66 +2,7 @@ import React, { Component } from 'react';
 import {Synth} from 'tone';
 import {connect} from 'react-redux';
 import {initSynth} from '../actions';
-
-class Keyboard extends Component {
-    constructor(props) {
-        super(props);
-
-        this.keyMap = {
-            65: 'C3',
-            87: 'C#3',
-            83: 'D3',
-            69: 'D#3',
-            68: 'E3',
-            70: 'F3',
-            84: 'F#3',
-            71: 'G3',
-            89: 'G#3',
-            90: 'G#3',
-            72: 'A3',
-            85: 'A#3',
-            74: 'B3'
-        }
-
-        this.notes = Object.values(this.keyMap);
-    }
-
-    onKeyUp(e) {
-
-    }
-
-    onKeyDown(e) {
-
-    }
-
-    onMouseDown(e) {
-
-    }
-
-    onMouseUp(e) {
-        
-    }
-
-    componentDidMount() {
-        document.addEventListener('keydown', this.onKeyDown);
-        document.addEventListener('keyup', this.onKeyUp);
-        document.addEventListener('mousedown', this.onMouseDown);
-        document.addEventListener('mouseup', this.onMouseUp);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('keydown', this.onKeyDown);
-        document.removeEventListener('keyup', this.onKeyUp);
-        document.removeEventListener('mousedown', this.onMouseDown);
-        document.removeEventListener('mouseup', this.onMouseUp);
-    }
-
-    render() {
-        return (
-            <p>keyboard</p>
-        )
-    }
-}
+import Keyboard from './Keyboard';
 
 class Synthesizer extends Component {
     componentDidMount() {
