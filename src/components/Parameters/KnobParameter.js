f027913476f5194b9d91bd0675daab53b3cc474c
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2018-02-20 22:57:06 +0100

    Font changes

--- src/components/Parameters/KnobParameter.js
+++ src/components/Parameters/KnobParameter.js
@@ -1,15 +1,18 @@
-import React, {Component} from 'react';
-import {findDOMNode} from 'react-dom';
+import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import * as d3 from 'd3';
 import styled from 'styled-components';
 
-import {saturateZeroOne, saturatePercentage} from '../../helpers/saturateValue';
+import {
+    saturateZeroOne,
+    saturatePercentage
+} from '../../helpers/saturateValue';
 import config from '../../synth/config';
 import calculateAngle from '../../helpers/calculateAngle';
 import StyledParameter from '../styled/StyledParameter';
 
-const {colors} = config;
-const StyledSVGWrapper = styled.div `
+const { colors } = config;
+const StyledSVGWrapper = styled.div`
     width: ${props => props.styledWidth}%;
     height: ${props => props.styledHeight}%;
     position: relative;
@@ -31,13 +34,14 @@ export default class KnobParameter extends Component {
     };
 
     calculateMouseAngle(mouse) {
-        return calculateAngle(mouse[0] - this.center.x, this.center.y - mouse[1]);
+        return calculateAngle(
+            mouse[0] - this.center.x,
+            this.center.y - mouse[1]
+        );
     }
 
     fixFloatValue(val) {
-        return val % 1 === 0
-            ? val
-            : parseFloat(val).toFixed(2);
+        return val % 1 === 0 ? val : parseFloat(val).toFixed(2);
     }
 
     handleDonutMouseClick = mouse => {
@@ -48,32 +52,23 @@ export default class KnobParameter extends Component {
          * Calculate the total available values going
          * from min to max (for -100 to 100 this would be 200)
          */
-        const total = this.props.min < 0
-            ? this.props.max + Math.abs(this.props.min)
-            : this.props.max - this.props.min;
+        const total =
+            this.props.min < 0
+                ? this.props.max + Math.abs(this.props.min)
+                : this.props.max - this.props.min;
 
-        const calculatedValueFromPercentage = percentageFromAngle * (total / 100);
+        const calculatedValueFromPercentage =
+            percentageFromAngle * (total / 100);
 
         // If min is under 100 it should be added from there and not from zero
         const value = this.props.min + calculatedValueFromPercentage;
 
-        this
-            .props
-            .setParameter(this.props.module, this.props.param, value);
+        this.props.setParameter(this.props.module, this.props.param, value);
     };
 
     handleContainerMouseMoveDrag = () => {
-        d3
-            .event
-            .preventDefault();
-        const {
-            max,
-            min,
-            value,
-            setParameter,
-            module,
-            param
-        } = this.props;
+        d3.event.preventDefault();
+        const { max, min, value, setParameter, module, param } = this.props;
         const oneth = max / 100;
 
         /**
@@ -85,20 +80,21 @@ export default class KnobParameter extends Component {
         const isAboveMin = value >= min;
         const isInsideParameterBoundaries = isBelowMax && isAboveMin;
 
-        if (!isInsideParameterBoundaries) 
-            setParameter(module, param, !isBelowMax
-                ? max
-                : min);
-        
-        const hasMovedVertically = d3.event.movementY < 0 || d3.event.movementY > 0;
-        const hasMovedHorizontally = d3.event.movementX < 0 || d3.event.movementX > 0;
+        if (!isInsideParameterBoundaries)
+            setParameter(module, param, !isBelowMax ? max : min);
 
-        if (isInsideParameterBoundaries && (hasMovedHorizontally || hasMovedVertically)) {
+        const hasMovedVertically =
+            d3.event.movementY < 0 || d3.event.movementY > 0;
+        const hasMovedHorizontally =
+            d3.event.movementX < 0 || d3.event.movementX > 0;
+
+        if (
+            isInsideParameterBoundaries &&
+            (hasMovedHorizontally || hasMovedVertically)
+        ) {
             const movement = hasMovedHorizontally
                 ? d3.event.movementX * -1
-                : hasMovedVertically
-                    ? d3.event.movementY
-                    : 0;
+                : hasMovedVertically ? d3.event.movementY : 0;
 
             // HINT: 1 / 100th of circle value (min and max) times movementY (times step
             // prop?)
@@ -132,18 +128,14 @@ export default class KnobParameter extends Component {
         const donut = container
             .append('g')
             .attr('transform', `translate(${width / 2}, 54)`)
-            .on('mouseover', function (data, i) {
-                d3
-                    .select(this)
-                    .style('cursor', 'crosshair');
+            .on('mouseover', function(data, i) {
+                d3.select(this).style('cursor', 'crosshair');
             })
-            .on('mouseout', function (data, i) {
-                d3
-                    .select(this)
-                    .style('cursor', 'pointer');
+            .on('mouseout', function(data, i) {
+                d3.select(this).style('cursor', 'pointer');
                 // parameter.transition().style('fill', colors.white);
             })
-            .on('click', function () {
+            .on('click', function() {
                 self.handleDonutMouseClick(d3.mouse(this));
             });
 
@@ -152,51 +144,31 @@ export default class KnobParameter extends Component {
             y: donut.attr('height') / 2
         };
 
-        container.on('mousedown', () => {
-            d3
-                .event
-                .preventDefault();
-            this.setState({isDragging: true});
-        })
-            .on('mouseover', function () {
-                d3
-                    .select(this)
-                    .style('cursor', 'all-scroll');
-                self
-                    .valueEl
-                    .transition()
-                    .style('fill', colors.white);
-                self
-                    .foreground
-                    .transition()
-                    .style('fill', '#ff8a2d');
-                findDOMNode(self.param)
-                    .classList
-                    .add('active');
+        container
+            .on('mousedown', () => {
+                d3.event.preventDefault();
+                this.setState({ isDragging: true });
+            })
+            .on('mouseover', function() {
+                d3.select(this).style('cursor', 'all-scroll');
+                self.valueEl.transition().style('fill', colors.white);
+                self.foreground.transition().style('fill', '#ff8a2d');
+                findDOMNode(self.param).classList.add('active');
             })
             .on('mousemove', () => {
-                if (!this.state.isDragging) 
-                    return;
-                
+                if (!this.state.isDragging) return;
+
                 this.handleContainerMouseMoveDrag();
             })
             .on('mouseup', () => {
-                this.setState({isDragging: false});
+                this.setState({ isDragging: false });
             })
             .on('mouseleave', () => {
-                d3
-                    .event
-                    .stopPropagation();
-                this
-                    .valueEl
-                    .transition()
-                    .style('fill', colors.lightGray);
-                this.setState({isDragging: false});
-                findDOMNode(this.param)
-                    .classList
-                    .remove('active');
-                this
-                    .foreground
+                d3.event.stopPropagation();
+                this.valueEl.transition().style('fill', colors.lightGray);
+                this.setState({ isDragging: false });
+                findDOMNode(this.param).classList.remove('active');
+                this.foreground
                     .transition(550)
                     .style('fill', `rgba(${colors.primary}, 1)`);
             });
@@ -204,7 +176,7 @@ export default class KnobParameter extends Component {
         // BACKGROUND
         donut
             .append('path')
-            .datum({endAngle: this.tau})
+            .datum({ endAngle: this.tau })
             .style('fill', 'rgba(0,0,0,0.2)')
             .attr('d', this.arc);
 
@@ -228,23 +200,25 @@ export default class KnobParameter extends Component {
             .attr('font-family', 'Rubik Light')
             .style('font-size', '0.45em');
 
-        this
-            .valueEl
-            .attr('y', donut.node().getBBox().height / 2 + this.valueEl.node().getBBox().height / 0.28);
-        this
-            .valueEl
-            .attr('x', width / 2 - this.valueEl.attr('width') / 2);
+        this.valueEl.attr(
+            'y',
+            donut.node().getBBox().height / 2 +
+                this.valueEl.node().getBBox().height / 0.28
+        );
+        this.valueEl.attr('x', width / 2 - this.valueEl.attr('width') / 2);
     }
 
-    updateKnob(saturatedValue, {value}) {
+    updateKnob(saturatedValue, { value }) {
         const self = this;
-        this
-            .foreground
+        this.foreground
             .transition()
             .duration(900)
             .ease(d3.easePolyOut)
             .attrTween('d', d => {
-                const interpolate = d3.interpolate(d.endAngle, saturatedValue * this.tau);
+                const interpolate = d3.interpolate(
+                    d.endAngle,
+                    saturatedValue * this.tau
+                );
 
                 return t => {
                     d.endAngle = interpolate(t);
@@ -252,27 +226,28 @@ export default class KnobParameter extends Component {
                 };
             });
 
-        this
-            .valueEl
+        this.valueEl
             .transition()
             .duration(850)
             .ease(d3.easeSinOut)
-            .tween('text', function () {
+            .tween('text', function() {
                 const nextValue = self.fixFloatValue(value);
                 const interpolate = d3.interpolate(this.textContent, nextValue);
                 const prec = (nextValue + '').split('.');
-                const round = prec.length > 1
-                    ? Math.pow(10, prec[1].length)
-                    : 1;
+                const round =
+                    prec.length > 1 ? Math.pow(10, prec[1].length) : 1;
 
                 return t => {
-                    this.textContent = Math.round(interpolate(t) * round) / round;
+                    this.textContent =
+                        Math.round(interpolate(t) * round) / round;
                 };
             });
     }
 
     componentDidMount() {
-        this.renderKnob(saturateZeroOne(this.props.min, this.props.max, this.props.value));
+        this.renderKnob(
+            saturateZeroOne(this.props.min, this.props.max, this.props.value)
+        );
     }
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -280,20 +255,23 @@ export default class KnobParameter extends Component {
     }
 
     componentWillUpdate(nextProps, nextState) {
-        this.updateKnob(saturateZeroOne(this.props.min, this.props.max, nextProps.value), nextProps);
+        this.updateKnob(
+            saturateZeroOne(this.props.min, this.props.max, nextProps.value),
+            nextProps
+        );
     }
 
     render() {
         return (
             <StyledSVGWrapper
                 styledWidth={this.props.width}
-                styledHeight={this.props.height}>
-                <svg ref={node => (this.node = node)}/>
+                styledHeight={this.props.height}
+            >
+                <svg ref={node => (this.node = node)} />
                 <StyledParameter
                     refProp={param => (this.param = param)}
-                    param={this.props.name
-                    ? this.props.name
-                    : this.props.param}/>
+                    param={this.props.name ? this.props.name : this.props.param}
+                />
             </StyledSVGWrapper>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,51 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 
-import {injectGlobal} from 'styled-components';
+import { injectGlobal } from 'styled-components';
 import './css/index.css';
 import './scene/';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 
-injectGlobal `
-    @font-face {
-        font-family: 'Rubik Bold';
-        src: url('../fonts/Rubik/Rubik-Bold.ttf');
-    }
-    @font-face {
-        font-family: 'Rubik Italic';
-        src: url('../fonts/Rubik/Rubik-Italic.ttf');
-    }
-    @font-face {
-        font-family: 'Rubik Light';
-        src: url('../fonts/Rubik/Rubik-Light.ttf');
-    }
-    @font-face {
-        font-family: 'Rubik Medium';
-        src: url('../fonts/Rubik/Rubik-Medium.ttf');
-    }
-    @font-face {
-        font-family: 'Rubik Regular';
-        src: url('../fonts/Rubik/Rubik-Regular.ttf');
-    }
-
-    @font-face {
-        font-family: 'Trirong Thin';
-        src: url('../fonts/Trirong/Trirong-Thin.ttf');
-    }
-    @font-face {
-        font-family: 'Trirong Thin Italic';
-        src : url('../fonts/Trirong/Trirong-ThinItalic.ttf');
-    }
-    @font-face {
-        font-family: 'Trirong Medium Italic';
-        src : url('../fonts/Trirong/Trirong-MediumItalic.ttf');
-    }
-
-    
-
+injectGlobal`
     html, body {
         background-color: #fff;
         position: relative;
@@ -72,6 +36,8 @@ injectGlobal `
 
 ReactDOM.render(
     <Provider store={store}>
-    <App/>
-</Provider>, document.querySelector('.synth'));
+        <App />
+    </Provider>,
+    document.querySelector('.synth')
+);
 registerServiceWorker();
